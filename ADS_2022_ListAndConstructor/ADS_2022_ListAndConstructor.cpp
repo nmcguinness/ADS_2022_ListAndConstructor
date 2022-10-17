6ff3e134555f523f1eb526939098534787595e40
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-10-17 14:52:57 +0100

    Added emplace and print

--- ADS_2022_ListAndConstructor/ADS_2022_ListAndConstructor.cpp
+++ ADS_2022_ListAndConstructor/ADS_2022_ListAndConstructor.cpp
@@ -49,6 +49,21 @@ int main()
 
 	list<Point2D> ptList;
 
-	Point2D p1(1, 2);
-	ptList.push_back(p1);
+	//technique 1 - make separate object and pass in
+	//Point2D p1(1, 2);
+	//ptList.push_back(p1);
+
+	//technique 2 - make object in place and pass in
+	//ptList.push_back(Point2D(3, 4));
+
+	//technique 3 - add object to list using EMPLACE
+	ptList.emplace_back(5, 6);
+
+	//technique 4 - use EMPLACE with iterator
+	list<Point2D>::const_iterator iter = ptList.begin();
+	ptList.emplace(iter, 7, 8);
+
+	//lets check that our EMPLACE function added in correct location
+	for (Point2D pt : ptList)
+		cout << pt << endl;
 }
