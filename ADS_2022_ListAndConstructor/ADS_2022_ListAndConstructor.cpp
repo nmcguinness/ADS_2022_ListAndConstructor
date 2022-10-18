14319e31afcc09949ac4c99cc752a0ec3a645a47
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-10-18 09:30:02 +0100

    Added print demo

--- ADS_2022_ListAndConstructor/ADS_2022_ListAndConstructor.cpp
+++ ADS_2022_ListAndConstructor/ADS_2022_ListAndConstructor.cpp
@@ -12,34 +12,39 @@ void demoIterators();
 
 int main()
 {
-	demoPushBackOrFront();
+	//demoPushBackOrFront();
 	demoPrintFromList();
-	demoPrintAsReference();
+	//demoPrintAsReference();
 	//demoEmplace();
 	//demoIterators();
 }
 
 void demoPushBackOrFront()
 {
+	list<Point2D> pList;
+	pList.push_back(Point2D(3, 4));
 }
 void demoPrintFromList()
 {
-}
-void demoPrintAsReference() {
-}
-
-void demoEmplace()
-{
-	//Point2D p1(1, 2);
 	list<Point2D> pList;
-	//pList.push_back(Point2D(3, 4));
-
 	pList.emplace_front(3, 4);
 	pList.emplace_front(6, 8);
 	pList.emplace_front(9, 12);
 
 	for (Point2D p : pList)
 		cout << p << endl;
+}
+void demoPrintAsReference() {
+}
+void demoEmplace()
+{
+	list<Point2D> pList;
+	pList.emplace_front(3, 4);
+	pList.emplace_front(6, 8);
+	pList.emplace_front(9, 12);
+
+	//for (Point2D p : pList)
+	//	cout << p << endl;
 }
 void demoIterators()
 {
